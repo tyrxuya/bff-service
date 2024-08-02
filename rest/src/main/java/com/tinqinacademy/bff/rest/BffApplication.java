cbfc829ab4f1ef5fb3b091bc00cc2b02aa31a3ca
Ivan Georgiev <ivan2003444@gmail.com>
2024-08-02 13:09:37 +0300

    fixed pom files

--- rest/src/main/java/com/tinqinacademy/bff/rest/BffApplication.java
+++ rest/src/main/java/com/tinqinacademy/bff/rest/BffApplication.java
@@ -2,12 +2,8 @@ package com.tinqinacademy.bff.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.tinqinacademy.authentication"})
-@EnableJpaRepositories(basePackages = {"com.tinqinacademy.authentication.persistence.repositories"})
-@EntityScan(basePackages = {"com.tinqinacademy.authentication.persistence.entities"})
 public class BffApplication {
 
     public static void main(String[] args) {
