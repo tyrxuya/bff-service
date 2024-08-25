6225eb67b613259f652fa3e9de74cb7f9303644d
Ivan Georgiev <ivan2003444@gmail.com>
2024-08-26 00:41:55 +0300

    Removed unnecessary operations

--- api/src/main/java/com/tinqinacademy/bff/api/operations/createuser/CreateUser.java
+++
@@ -1,6 +0,0 @@
-package com.tinqinacademy.bff.api.operations.createuser;
-
-import com.tinqinacademy.bff.api.base.OperationProcessor;
-
-public interface CreateUser extends OperationProcessor<CreateUserRequest, CreateUserResponse> {
-}
--- api/src/main/java/com/tinqinacademy/bff/api/operations/getuser/GetUser.java
+++
@@ -1,6 +0,0 @@
-package com.tinqinacademy.bff.api.operations.getuser;
-
-import com.tinqinacademy.bff.api.base.OperationProcessor;
-
-public interface GetUser extends OperationProcessor<GetUserRequest, GetUserResponse> {
-}
