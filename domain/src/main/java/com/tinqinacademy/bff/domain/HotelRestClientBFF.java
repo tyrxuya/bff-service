9df3166b20764eb173ef64bf87bdcf0038513f9d
Ivan Georgiev <ivan2003444@gmail.com>
2024-08-26 00:42:19 +0300

    Made hotel client work with OkHttpClient

--- domain/src/main/java/com/tinqinacademy/bff/domain/HotelRestClientBFF.java
+++ domain/src/main/java/com/tinqinacademy/bff/domain/HotelRestClientBFF.java
@@ -1,10 +1,11 @@
 package com.tinqinacademy.bff.domain;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tinqinacademy.hotel.restexport.HotelRestExport;
+import com.tinqinacademy.hotel.restexport.HotelClient;
 import feign.Feign;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
+import feign.okhttp.OkHttpClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
@@ -15,17 +16,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class HotelRestClientBFF {
     private final ApplicationContext applicationContext;
+    private final OkHttpClient client;
 
     @Value("${hotel.client.url}")
     private String HOTEL_URL;
 
     @Bean
-    public HotelRestExport getHotelRestClient() {
+    public HotelClient getHotelRestClient() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.findAndRegisterModules();
         return Feign.builder()
+                .client(client)
                 .encoder(new JacksonEncoder(applicationContext.getBean(ObjectMapper.class)))
                 .decoder(new JacksonDecoder(applicationContext.getBean(ObjectMapper.class)))
-                .target(HotelRestExport.class, HOTEL_URL);
+                .target(HotelClient.class, HOTEL_URL);
     }
 }
